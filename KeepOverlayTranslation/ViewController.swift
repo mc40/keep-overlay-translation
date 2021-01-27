0073f3efd0cd684890ed9db50b9d52ad7d43befe
MasonChan <masonchan@fundpark.com>
2021-01-27 23:41:16 +0800

    Initial Commit

---
+++ KeepOverlayTranslation/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KeepOverlayTranslation
+//
+//  Created by Chung Man Chan on 27/1/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

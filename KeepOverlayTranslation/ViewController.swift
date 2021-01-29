bcd80a785aaa9ae6f171ee69b23668c28e38b604
MasonChan <masonchan@fundpark.com>
2021-01-29 16:03:31 +0800

    Initial project version

--- KeepOverlayTranslation/ViewController.swift
+++ KeepOverlayTranslation/ViewController.swift
@@ -6,18 +6,28 @@
 //
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let myURL = URL(string:"https://translate.google.com")
+        let myRequest = URLRequest(url: myURL!)
+        self.webView.load(myRequest)
         // Do any additional setup after loading the view.
     }
-
+    override func viewDidAppear() {
+        view.window?.level = .floating
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
+            
         }
     }
 
